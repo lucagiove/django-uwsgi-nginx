56df8ae0c5c0b91f17ed0caa55ce72f6c733f98c
Luca Giovenzana <l.giovenzana@endian.com>
2017-07-29 10:18:06 +0200

    changed from sed to envsubst
    - fixed init script

--- initialize-srv.sh
+++ initialize-srv.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# if they exist already nothing will change
-mkdir -p /srv/$APP_NAME/media
-mkdir -p /srv/$APP_NAME/static
-
 # if app folder does not exists already
+# FIXME seems not to work
 if [[ ! -d /srv/$APP_NAME ]]; then
     touch /srv/$APP_NAME/media/PUT_HERE_YOUR_GENERATED_DATA
     touch /srv/$APP_NAME/static/PUT_HERE_YOUR_STATIC_DATA
-    # FIXME chown to www-data?
+    chown -R :www-data /srv/$APP_NAME/
+    chmod -R g=rX /srv/$APP_NAME/
     # TODO if empty git clone
     # django migrate or django create new
-    exit 0
 fi
+# those folder are required by nginx, if they exist already nothing will change
+mkdir -p /srv/$APP_NAME/media
+mkdir -p /srv/$APP_NAME/static
 exit 0
